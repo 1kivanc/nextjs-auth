4d86dfca1847b480473ca864864322055d799257
1kivanc <kivanc0556@gmail.com>
2024-11-24 19:50:08 +0300

    sign up page

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,84 @@
+"use client"
+
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import Link from 'next/link'
 import React from 'react'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+ 
+const formSchema = z.object({
+  email: z.string().min(2).max(50),
+  password: z.string().min(2).max(50),
+})
+
 
 export default function SignUp() {
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: ""
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
   return (
-    <div>
-        <h1>Sign Up</h1>
-    </div>
+    <Card className='w-full max-w-md'>
+  <CardHeader>
+    <CardTitle>Sign Up</CardTitle>
+    <CardDescription>Create your account to get started</CardDescription>
+  </CardHeader>
+  <CardContent>
+  <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Name@gmail.com" {...field} />
+              </FormControl>
+              
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type='password' placeholder="Enter your password" {...field} />
+              </FormControl>
+              
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  </CardContent>
+  <CardFooter className='flex justify-center'>
+    <p className='text-sm text-muted-foreground'>
+      Already have an account?{' '}
+      <Link href="/sign-in" className="text-primary hover:underline">
+        Sign In
+      </Link>
+    </p>
+  </CardFooter>
+</Card>
+ 
   )
 }
